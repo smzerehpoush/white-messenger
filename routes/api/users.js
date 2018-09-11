54dc82a292c471b9cbba48600559d2a24dde5801
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-11 10:06:28 +0430

    implement services: register, login

---
+++ routes/api/users.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/User')
+
+router.post('/', async (req, res) => {
+    let user = await User
+        .checkUser(req.body.phoneNumber)
+    res.send(user)
+})
+
+router.post('/register', async (req, res) => {
+    console.log(req.body)
+    let phoneNumber = req.body.phoneNumber
+    let user = await User.checkUser(phoneNumber) 
+    if (!user) {
+        let user = new User({
+            phoneNumber,
+            username: phoneNumber
+        })
+        User.addUser(user, (err) => {
+            if (err)
+                console.log('failed to create user :', err)
+            else {
+                res.send(user)
+            }
+        })
+    }
+
+})
+router.post('/login', async (req, res) => {
+
+    let user = await User.findOne({ username: req.body.username, password: req.body.password })
+    if (!user) {
+
+        res.send('token')
+    }
+    else {
+        res.status(400).send('user not exists')
+    }
+})
+
+
+module.exports = router
