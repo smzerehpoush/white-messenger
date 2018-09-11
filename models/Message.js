3011a8788748a6dc246f73215a0660223cc89055
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-11 14:01:57 +0430

    add new model : Message
    text , sender,reciever

---
+++ models/Message.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const MessageSchema = new mongoose.Schema({
+    text:{
+        type:String,
+    },
+    // file:{
+    //     type:String
+    // },
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    reciever:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+})
