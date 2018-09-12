c4b763d40e336e4c326bca362a2ee14a517f745b
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-12 12:23:32 +0430

    add new model : group

---
+++ models/Group.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const groupSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId
+        , ref: 'User'
+    }
+    , admins: [{
+        type: mongoose.Schema.Types.ObjectId
+        , ref: 'User'
+    }]
+    , members: [{
+        type: mongoose.Schema.Types.ObjectId
+        , ref: 'User'
+    }]
+})
+
+const Group = mongoose.models('Group', groupSchema)
+module.exports = Group
