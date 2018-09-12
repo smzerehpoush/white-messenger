16fa10a7215016a673e898ff038665fea3d53932
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-12 12:06:11 +0430

    add new model : channelMessages

---
+++ models/ChannelMessages.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const channelMessagesSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId
+        , ref: 'User'
+    }
+    , reciever: {
+        type: mongoose.Schema.Types.ObjectId
+        , ref: 'Channel'
+    }
+    , message: {
+        type: mongoose.Schema.Types.ObjectId
+        , ref: 'Message'
+    }
+})
