fc34ef76561b26d9fc9342c0080c8c4afc435a89
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-12 14:56:42 +0430

    remove test sockets

--- socket.js
+++ socket.js
@@ -2,42 +2,31 @@ const express = require('express');
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-let userCount = 0
+const User = require('./models/User')
 
 
 io.use(async function(socket,next) {
 
-    console.log('a user connected : ' + socket.id)
-    const handshakeData = socket.request
-    const userId = handshakeData._query['b']
-    const clientId = handshakeData._query['c']
-    const req = {userId,clientId}
+    const userId = socket.request._query['user_id']
+    const socketId = socket.request._query['socket_id']
+    const mac = socket.request._query['mac']
+    
+    const user = await User.findById(userId)
+    if(user){
+        console.log(user)
+    }
     console.log(req)
     next()
 })
 
 
 io.on('connection', function(socket){
-    console.log('+: a user connected ...',socket.id)
-    console.log('user count : ',userCount)
-    userCount++
-
-    socket.emit('newUser',{description:'Welcome!'})
-    socket.broadcast.emit('newUser',{description:'user count '+userCount})
+    console.log(socket.id, 'connected')
     
-    socket.on('testing',function(){
-        io.sockets.emit('broadcast',{description:'user count '+userCount})
-        socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
-    })
-    socket.on('test2',function(data){
-        io.sockets.emit('test2', data)
-        // socket.send('test2')
-    })
+
     
     socket.on('disconnect',function(){
-        userCount--
-        io.sockets.emit('broadcast',{description:'a user lef.number of users : '+userCount})
-        console.log('-: a user disconnected ...')
+        console.log('-:  user',socket.id,' disconnected ...')
     })
 })
 const port = 3001
