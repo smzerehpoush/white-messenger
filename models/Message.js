09e1eef12728c8ad58c7395b25ba5925c58eed06
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-12 11:37:55 +0430

    add 2 entities : file and fileType

--- models/Message.js
+++ models/Message.js
@@ -4,9 +4,13 @@ const MessageSchema = new mongoose.Schema({
     text: {
         type: String,
     },
-    // file:{
-    //     type:String
-    // },
+    file: {
+        type: String
+    },
+    fileType: {
+        type: String,
+        enum: ['mp3', 'mp4', 'file', 'voice']
+    },
     sender: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
