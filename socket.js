f8e016e1b82ce220c53ce9dd2e4adbf90846a926
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-12 11:31:52 +0430

    using io.use to get data from query string

--- socket.js
+++ socket.js
@@ -3,11 +3,28 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 let userCount = 0
+
+
+io.use(async function(socket,next) {
+
+    console.log('a user connected : ' + socket.id)
+    const handshakeData = socket.request
+    const userId = handshakeData._query['b']
+    const clientId = handshakeData._query['c']
+    const req = {userId,clientId}
+    console.log(req)
+    next()
+})
+
+
 io.on('connection', function(socket){
     console.log('+: a user connected ...',socket.id)
+    console.log('user count : ',userCount)
     userCount++
+
     socket.emit('newUser',{description:'Welcome!'})
     socket.broadcast.emit('newUser',{description:'user count '+userCount})
+    
     socket.on('testing',function(){
         io.sockets.emit('broadcast',{description:'user count '+userCount})
         socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
