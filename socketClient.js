95bfb3fe3e732b7f1d6befb5f24ecdb650b7b8cb
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-09-13 09:12:46 +0430

    add socket client class

--- socketClient.js
+++ socketClient.js
@@ -1,1 +1,10 @@
-const io =require('socket.io-client')
+const socket = require('socket.io-client')('http://127.0.0.1:3001')
+// const socket = io.connect
+setTimeout(() => {
+
+    console.log('test...');
+    socket.on('connection', function () {
+        console.log('connected...');
+
+    });
+}, 2000)
