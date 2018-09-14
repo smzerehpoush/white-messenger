604e945c332d9a628c3e87d28bd6646863a76fdd
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-09-14 11:41:31 +0430

    using readline lib to send input data to server for chatting!

--- client.js
+++ client.js
@@ -1,7 +1,21 @@
-let socket = require('socket.io-client')('http://localhost:3001?user_id=5b99f43150fd6337a440817d&client_id=5b99f43150fd6337a440817e&socket_id=12345678');
-socket.on('connect', function(){
-    console.log('connected...');
-    
+let socket = require('socket.io-client')('http://localhost:3001?user_id=5b99f43150fd6337a440817d&client_id=5b99f43150fd6337a440817e');
+// let socket = require('socket.io-client')('http://localhost:3001?');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
 });
-socket.on('event', function(data){});
-socket.on('disconnect', function(){});
+
+rl.on('line', function (line) {
+    socket.emit('privateChat', line)
+})
+
+socket.on('connect', function () {
+    console.log('connected...');
+
+});
+socket.on('privateChat', function (data) {
+    console.log(data)
+});
+socket.on('disconnect', function () {});
