9d625192496fae271dc2545e0114fc7e83c40804
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-15 11:04:03 +0430

    fill the variables

--- client1.js
+++ client1.js
@@ -1,7 +1,7 @@
 // let chat = require('socket.io-client')('http://localhost:3001/chat?user_id=5b99f43150fd6337a440817d&client_id=5b99f43150fd6337a440817e');
-const user_id_1 = ''
-const user_id_2 = ''
-const client_id_1 = ''
+const user_id_1 = '5b9ca5f1fe0c7826f8e2a3d9'
+const user_id_2 = '5b9ca5f2fe0c7826f8e2a3db'
+const client_id_1 = '5b9ca5f1fe0c7826f8e2a3da'
 let chat = require('socket.io-client')('http://localhost:3001/?user_id=' + user_id_1 + '&client_id=' + client_id_1);
 const readline = require('readline');
 const rl = readline.createInterface({
--- client2.js
+++ client2.js
@@ -1,7 +1,7 @@
 // let chat = require('socket.io-client')('http://localhost:3001/chat?user_id=5b9b784739a1366cb5b8ab4d&client_id=5b99f43150fd6337a4408177');
-const user_id_1 = ''
-const user_id_2 = ''
-const client_id_1 = ''
+const user_id_1 = '5b9ca5f2fe0c7826f8e2a3db'
+const user_id_2 = '5b9ca5f1fe0c7826f8e2a3d9'
+const client_id_1 = '5b9ca5f2fe0c7826f8e2a3dc'
 let chat = require('socket.io-client')('http://localhost:3001/?user_id=' + user_id_1 + '&client_id=' + client_id_1);
 const readline = require('readline');
 const rl = readline.createInterface({
